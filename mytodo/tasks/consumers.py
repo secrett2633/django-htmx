169a7fbbbb86a7f8e219732cfa30fb6411cba70c
secrett2633 <secrett2633@kakao.com>
2025-03-15 18:14:22 +0900

    feat: 챗봇 ollama 모델로 동작하도록 변경경

--- mytodo/tasks/consumers.py
+++ mytodo/tasks/consumers.py
@@ -1,4 +1,5 @@
 import asyncio
+import ollama
 
 from channels.generic.http import AsyncHttpConsumer
 
@@ -15,11 +16,20 @@ class ChatConsumer(AsyncHttpConsumer):
         ])        
         
         user_message = self.scope['url_route']['kwargs']['message']
-        for i, data in enumerate("SSE 응답 테스트입니다. '" + user_message + "' 메세지에 대한 LLM 응답은 추후에 추가될 예정입니다."):
-            if data == " ":
-                data = "&nbsp;"
-            await self.send_body(f"id: {i}\nevent: message\ndata: {data}\n\n".encode("utf-8"), more_body=True)
-            await asyncio.sleep(0.05)        
-        
+    
+        stream = ollama.chat(
+            model='deepseek-r1:32b',
+            messages=[{'role': 'user', 'content': user_message}],
+            stream=True,
+        )
+
+        for chunk in stream:
+            data = chunk['message']['content']
+            data = data.replace(" ", "&nbsp;")
+            data = data.replace("\n", "<br/>")
+
+            await self.send_body(f"event: message\ndata: {data}\n\n".encode("utf-8"), more_body=True)
+            await asyncio.sleep(0)
+            
         chat_id = self.scope['url_route']['kwargs']['chat_id']
         await self.send_body(f'event: message\ndata: <div id="chat-sse-listener-{chat_id}" hx-swap-oob="true"></div>\n\n'.encode("utf-8"), more_body=False)
